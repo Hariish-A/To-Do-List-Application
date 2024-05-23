587809d9a87d6e48a99374e47ef228a1a153dc37
Hariish-A <hariishero@gmail.com>
2024-05-23 09:45:04 +0530

    Add project files.

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using MauiApp1.ViewModel;
+using Microsoft.Extensions.Logging;
+
+namespace MauiApp1
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+            builder.Services.AddSingleton<IConnectivity>(Connectivity.Current);
+
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<MainViewModel>();
+
+            builder.Services.AddTransient<DetailPage>();
+            builder.Services.AddTransient<DetailViewModel>();
+
+
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
